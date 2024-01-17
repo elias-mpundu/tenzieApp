61ef2a63bd48ef828819e51ab45db01d2821542c
Elias Mpundu <97706618+elias-mpundu@users.noreply.github.com>
2024-01-16 19:12:46 -0600

    App can refresh when game is over and the useEffect change new state (tenzies) to true in the event all dice are identical.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,30 @@ import "./App.css";
 export default function App() {
   //Note: When app is first run, state is initialized by calling the allNewDice function.
   const [dice, setDice] = useState(allNewDice);
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    //Recall: useEffect is run after browser has rendered. 
+    //The idCheck intakes the an array of objects and creates a set. 
+    //Since a set cannot have identical items, it will ignore all repetitions hence making the length/size === 1.
+    //Test this for both the value and isHeld to make sure all dice are selected and the same value and return you desired condition because the game has been won. 
+    function idCheck(array, property){
+      const uniqueProp = new Set(array.map(die => die[property]))
+      console.log(uniqueProp, "hello")
+      return uniqueProp.size === 1;
+    }
+
+    if(idCheck(dice, "value") && idCheck(dice, "isHeld")){
+      setTenzies(true)
+    }
+    console.log("rerun")
+  }, [dice])
+
+
+  function newGame() {
+    setDice(allNewDice)
+    setTenzies(false)
+  }
 
 
   //newDiceLoop generates a new value for each die. Isolating this behavior makes it easier to malnipulate the result 
@@ -73,9 +97,19 @@ export default function App() {
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div id="dice-container">{diceValues}</div>
-      <button className="roll-dice" onClick={rollDice}>
-        Roll
-      </button>
+      {tenzies === false ? (
+          <button className="roll-dice" onClick={rollDice}>
+            Roll
+          </button>
+        ) : (
+          <button className="roll-dice" onClick={newGame}>
+            New Game
+          </button>
+        )
+      }
+      {/* <button className="roll-dice" onClick={rollDice}>
+        {tenzies === true ? <>New Game</> : <>Roll</>}
+      </button> */}
     </main>
   );
 }
